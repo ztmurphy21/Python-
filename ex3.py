09a84ae5a472b1a504db8d94c3a5905a6ec8c4ce
Zachary Murphy <murphyz@my.wccc.edu>
2016-08-01 22:08:17 -0400

    Three ex's completed

---
+++ ex3.py
@@ -0,0 +1,25 @@
+#Output text
+print ("I will now count my chickens:")
+
+#Output text with calculations
+print ("Hens", 25.0 + 30.0 / 6.0)
+print ("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+#Output text
+print ("Now I will count the eggs")
+
+#Testing some conditions math wise
+print (3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0)
+print ("Is it true that 3+2 < 5-7?")
+print (3.0+2.0 < 5.0-7.0) 
+print ("What is 32+?", 3.0+2.0)
+print ("What is 5-7?", 5.0-7.0)
+print ("Oh, that is why it's false.")
+
+#Output text
+print ("How about some more.")
+
+#Testing some conditions math wise
+print ("Is it greater?", 5.0 < -2.0)
+print ("Is it greater or equal?", 5.0 >= -2.0)
+print ("Is it less or equal?", 5.0 <= -2.0)
---
+++ ex3sd3.py
@@ -0,0 +1,5 @@
+#Output description of calculation
+print ("I need to calulate how much money I will make in one week.")
+
+#calculation and output
+print ("$",40*50)
